e67afab4b6830413656020b49338488d9182de0e
arashigaoka <17226903+arashigaoka@users.noreply.github.com>
2024-01-02 14:18:46 +0900

    add bg image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import './App.css';
+import styles from './App.module.css';
+import bgCafe from './assets/bg-cafe.jpg';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <header className={styles.header}>
+      <img src={bgCafe} alt="cafe" className={styles.image} />
+    </header>
+  );
 }
 
 export default App;

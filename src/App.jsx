76f1e425c6c90a94ad8fcb6f277a4fe2497c11cd
arashigaoka <17226903+arashigaoka@users.noreply.github.com>
2024-01-03 16:43:13 +0900

    add markup

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,26 @@ import bgCafe from './assets/bg-cafe.jpg';
 
 function App() {
   return (
-    <header className={styles.header}>
-      <img src={bgCafe} alt="cafe" className={styles.image} />
-    </header>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <img src={bgCafe} alt="cafe" className={styles.image} />
+      </header>
+      <div className={styles.contents}>
+        <h1>Our Collection</h1>
+        <p>
+          Introducing our Coffee Collection, a selection of unique coffees from
+          different roast types and origins, expertly roasted in small batches
+          and shipped fresh weekly.
+        </p>
+        <div className={styles.tabs}>
+          <input type="radio" id="all-products" name="filter-tab" checked />
+          <label htmlFor="all-products">All Products</label>
+
+          <input type="radio" id="available" name="filter-tab" />
+          <label htmlFor="available">Available Now</label>
+        </div>
+      </div>
+    </div>
   );
 }
 
